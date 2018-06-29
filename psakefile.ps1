c7ce57c4bcab4a941810f8ae9a173ad18e3cb6f1
Garo Yeriazarian <garo.yeriazarian@headspring.com>
2018-06-28 22:19:41 -0500

    Add msbuild calls and unit testing.

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,23 +1,58 @@
 properties {
-  $testMessage = 'Executed Test!'
-  $compileMessage = 'Executed Compile!'
-  $cleanMessage = 'Executed Clean!'
+	$projectName = "SampleProject"
+	
+	if(-not $version)
+    {
+        $version = "1.0.0.0"
+    }
+
+	$projectConfig = "Release"
+
+	# paths
+	$baseDir = resolve-path .\
+	$sourceDir = "$baseDir\src"
+	$dbDir = "$baseDir\src\Databases"	
+	$testResultsDir = "$baseDir\build\testResults"
+	$nugetDir = "$baseDir\tools\nuget"
+	$stageDir = "$baseDir\build\stage"
+	$packageDir = "$baseDir\build\package"
+	$octopusDir = "$baseDir\tools\octopus"
+	
+	# database related properties
+	$rhExe = "$baseDir\tools\roundhouse\tools\rh.exe"
+	$rhOutputDir = "$baseDir"
+	$rhVersionFile = "$dbDir\_BuildInfo.xml"
 }
 
 task default -depends Test
 
 task Test -depends Compile, Clean {
-  $testMessage
+	get-childitem src *.Tests -directory | foreach-object {
+		set-location $_.fullname
+		dotnet.exe fixie
+	}
+
+	set-location $baseDir
 }
 
 task Compile -depends Clean {
-  $compileMessage
+	RunMsBuild -target "rebuild"
 }
 
 task Clean {
-  $cleanMessage
+	RunMsBuild -target "clean"
 }
 
 task ? -Description "Helper to display task info" {
   Write-Documentation
+}
+
+task Info {
+	exec { dotnet.exe --info }
+	exec { dotnet.exe msbuild /version }
+}
+
+
+function RunMsBuild($target) {
+	exec { dotnet.exe msbuild /t:$target /v:q /m /p:Configuration=$projectConfig /nologo /nr:false $sourceDir\$projectName.sln }
 }
