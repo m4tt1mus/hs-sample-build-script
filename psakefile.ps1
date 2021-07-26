0fdf189b8ec5bd77fb246cf78c2856d84bc3aa5e
Emil Bsaibes <emil.bsaibes@headspring.com>
2021-07-26 16:10:06 -0500

    TECH-91 clean up script. Remove the artifiacts folder on Clean. Made sure naming aligns with folder name for artifacts.

--- psakefile.ps1
+++ psakefile.ps1
@@ -8,7 +8,7 @@ properties {
     $yearInitiated = '2018'
     $projectRootDirectory = "$(resolve-path .)"
     $publish = "$projectRootDirectory/Publish"
-	$artifact = "$projectRootDirectory/Artifacts"
+	$artifacts = "$projectRootDirectory/Artifacts"
     $testResults = "$projectRootDirectory/TestResults"
 }
 
@@ -36,14 +36,15 @@ task Publish -depends Compile -description "Publish the primary projects for dis
 }
 
 task Package -depends Compile -description "Package the primary project into Nuget package with version number" {
-	remove-directory-silently $artifact
-    exec { dotnet pack --configuration $configuration --nologo -p:"Product=$($product)" -p:"Copyright=$(get-copyright)" -p:"Version=$($version)" } -workingDirectory src
+	remove-directory-silently $artifacts
+    exec { dotnet pack --configuration $configuration --nologo -p:"Product=$($product)" -p:"Copyright=$(get-copyright)" -p:"Version=$($version)" -o:$artifacts} -workingDirectory src
 }
   
 task Clean -description "Clean out all the binary folders" {
     exec { dotnet clean --configuration $configuration /nologo } -workingDirectory src
     remove-directory-silently $publish
     remove-directory-silently $testResults
+    remove-directory-silently $artifacts
 }
   
 task ? -alias help -description "Display help content and possible targets" {
